1e4ae7a64113b6074b6e075c329e0af6417bd303
IT19008042 <63994867+SliitBoy@users.noreply.github.com>
2020-10-05 09:26:17 +0530

    Forum (#8)
    * Forum
    
    * Forum create
    
    * Forum commit 4
    
    * Forum project with create and view list
    
    * Forum project final changes
    
    Co-authored-by: notgimp <it19070322@my.sliit.lk>

--- app/src/main/java/com/simpl/krammer/forum/Forum.java
+++ app/src/main/java/com/simpl/krammer/forum/Forum.java
@@ -3,74 +3,48 @@ package com.simpl.krammer.forum;
 import java.util.ArrayList;
 
 public class Forum {
-    private String id;
-    private String Forum_title;
-    private String Forum_desc;
-    private ArrayList<String> replies;//Reply--UserID
+
+    private String forum_title;
+    private String forum_desc;
+   // private ArrayList<String> replies;//Reply--UserID
 
     public Forum(){
 
     }
 
-    public Forum(String Forum_title, String forum_desc) {
-        this.Forum_title = Forum_title;
-        Forum_desc = forum_desc;
+    public Forum(String Forum_title, String Forum_desc) {
+        this.forum_title = Forum_title;
+        this.forum_desc = Forum_desc;
     }
 
-    public Forum(String id, String Forum_title, String forum_desc) {
-        this.id = id;
-        this.Forum_title = Forum_title;
-        Forum_desc = forum_desc;
-    }
 
-    public Forum(String id, String Forum_title, String forum_desc, ArrayList<String> replies) {
-        this.id = id;
-        this.Forum_title = Forum_title;
-        Forum_desc = forum_desc;
-        this.replies = replies;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
+//    public Forum(String Forum_title, String Forum_desc, ArrayList<String> replies) {
+//        this.forum_title = Forum_title;
+//        this.forum_desc = Forum_desc;
+//        this.replies = replies;
+//    }
 
     public String getForum_title() {
-        return Forum_title;
+        return forum_title;
     }
 
-    public void setForum_title(String Forum_title) {
-        this.Forum_title = Forum_title;
+    public void setForum_title(String forum_title) {
+        this.forum_title = forum_title;
     }
 
     public String getForum_desc() {
-        return Forum_desc;
+        return forum_desc;
     }
 
     public void setForum_desc(String forum_desc) {
-        Forum_desc = forum_desc;
+        this.forum_desc = forum_desc;
     }
 
-    public ArrayList<String> getReplies() {
-        return replies;
-    }
-
-    public void setReplies(ArrayList<String> replies){
-        this.replies=replies;
-    }
-
-    public void addReply(String reply){
-        this.replies.add(reply);
-    }
-
-    public void removeReply(String reply){
-        this.replies.remove(reply);
-    }
-
-    public void removeReply(int replyindex){
-        this.replies.remove(replyindex);
-    }
+//    public ArrayList<String> getReplies() {
+//        return replies;
+//    }
+//
+//    public void setReplies(ArrayList<String> replies) {
+//        this.replies = replies;
+//    }
 }
---
+++ app/src/main/java/com/simpl/krammer/forum/ViewForumRecyclerViewAdapter.java
@@ -0,0 +1,56 @@
+package com.simpl.krammer.forum;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.simpl.krammer.R;
+import com.simpl.krammer.forum.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+public class ViewForumRecyclerViewAdapter extends RecyclerView.Adapter<ViewForumRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Forum> mValues;
+
+    public ViewForumRecyclerViewAdapter(List<Forum> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.forum_view_all_copy, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).getForum_title());
+        holder.mContentView.setText(mValues.get(position).getForum_desc());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public Forum mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.forum_title_copy);
+            mContentView = (TextView) view.findViewById(R.id.forum_descp_copy);
+        }
+
+    }
+}
