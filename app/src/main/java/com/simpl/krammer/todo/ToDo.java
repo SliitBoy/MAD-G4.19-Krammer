ef654b5c1e57af038249762e6012e9bede785d56
IT19008042 <63994867+SliitBoy@users.noreply.github.com>
2020-10-11 19:06:36 +0530

    To do list (#9)
    * ToDoList initial push. Added Create and Read
    
    * ToDoList initial Push Create,Read
    
    Co-authored-by: KaiProgramming <65773301+KaiProgramming@users.noreply.github.com>

---
+++ app/src/main/java/com/simpl/krammer/todo/ToDo.java
@@ -0,0 +1,30 @@
+package com.simpl.krammer.todo;
+
+public class ToDo {
+
+    private String task;
+    private String priority;
+
+    public ToDo(){
+    }
+
+    public ToDo(String task, String priority) {
+        this.task = task;
+        this.priority = priority;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+    public String getPriority(){
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
